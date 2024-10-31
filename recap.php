91056ea02840a52df770afab03a0101e6c9cd49d
Michaël HEID <michael123456222444666@gmail.com>
2024-10-31 14:57:53 +0100

    Add files via upload

--- recap.php
+++ recap.php
@@ -75,7 +75,7 @@ recap.php devra nous permettre d'afficher de manière organisée et exhaustive l
 
         echo "<tr>",
             "<td colspan = 4>Total Général : </td>", // cellule fusionnée de 4 cellules
-            "<td><strong>.number_format($totalGeneral, 2, ",", "&nbsp;")."&nbsp;€"</strong></td>,
+            "<td><strong>".number_format($totalGeneral, 2, ",", "&nbsp;")."&nbsp;€</strong></td>",
         "</tr>",
         "</tbody>",
         "</table>";
--- traitement.php
+++ traitement.php
@@ -6,21 +6,20 @@
     // Pour cela, une condition simple doit être effectuée :
     if(isset($_POST['submit'])) {
 
-
         // La fonction PHP filter_input() permet d'effectuer une validation ou un nettoyage de chaque donnée transmise par le formulaire, en employant divers filtres.
         // FILTER_SANITIZE_STRING (champ "name") : ce filtre supprime une chaîne de caractères de toute présence de caractères spéciaux et de toute balise HTML potentielle ou les encode.
         // FILTER_VALIDATE_FLOAT (champ "price") : validera le prix que s'il est un nombre à virgule (pas de texte ou autre…).
         // FILTER_FLAG_ALLOW_FRACTION est ajouté pour permettre l'utilisation du caractère "," ou "." pour la décimale.
         // FILTER_VALIDATE_INT (champ "qtt") : ne validera la quantité que si celle-ci est un nombre entier différent de zéro (qui est considéré comme nul).
-        $name = filter_input(INPUT,POST, "name", FILTER_SANITIZE_STRING);
-        $price = filter_input(INPUT,POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
-        $name = filter_input(INPUT,POST, "qtt", FILTER_VALIDATE_INT);
-
+        $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+        $qtt = filter_input(INPUT_POST, "qtt", FILTER_VALIDATE_INT);
+        
         // On vérifie si Les filtres ont tous fonctionné grâce à une nouvelle condition.
         // Sachant qu'un filtre renverra false ou null s'il échoue, et que nous ne pouvons pas anticiper la saisie de l'utilisateur à ce niveau, il suffit de vérifier implicitement si chaque variable contient une valeur jugée positive par PHP (du texte, des nombres, etc., autrement dit tout sauf false ou null ou 0). Voilà pourquoi la condition ne compare les variables à rien de précis.
         // Pour faire simple, on vérifie qu'on n'obtient pas du false, du null ou du 0 (pas de choses trop pourries)
         if($name AND $price AND $qtt) {
-
+        
 
             // on construit pour chaque produit un tableau associatif $product
             // organisation des données nous permettra d'afficher nos produits plus tard de manière plus efficace et de rendre notre code plus explicite.
@@ -37,7 +36,6 @@
             // on indique la clé "products" de ce tableau. Si cette clé n'existait pas auparavant (ex: l'utilisateur ajoute son tout premier produit), PHP la créera au sein de $_SESSION.
             // Les deux crochets "[]"sont un raccourci pour indiquer à cet emplacement que nous ajoutons une nouvelle entrée au futur tableau "products" associé à cette clé. $_SESSION["products"] doit être lui aussi un tableau afin d'y stocker de nouveaux produits par la suite.
             $_SESSION['products'][] = $product;
-            
 
         }
 
