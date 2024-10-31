34cd8cc49cc91dfab68558869cdda53754c73a4d
Michaël HEID <michael123456222444666@gmail.com>
2024-10-31 15:29:45 +0100

    Add files via upload

---
+++ coloration_rouge.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Ajout produit</title>
+    </head>
+    <body>
+
+        <h1>
+            Ajouter un produit
+        </h1>
+
+        <!-- la balise <form> comporte deux attributs.
+            1 : action indique la cible du formulaire, le fichier à atteindre lorsque l'utilisateur soumet le formulaire
+            2 : method précise par quelle méthode HTTP les données du formulaire sont transmises au serveur -->
+        
+
+    </body>
+</html>
